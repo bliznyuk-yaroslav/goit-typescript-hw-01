537ce561be395d3d28def4255f44d5cfe079eab5
bliznyuk-yaroslav <144949333+bliznyuk-yaroslav@users.noreply.github.com>
2024-05-26 10:41:32 +0300

    update

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -3,6 +3,6 @@ const username: string = "Max";
 const toggle: boolean = true;
 const empty: null = null;
 
-const callback = (a: number) => {
+const callback = (a: number): number => {
   return 100 + a;
 };
